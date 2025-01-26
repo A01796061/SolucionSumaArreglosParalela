da670972313fa964126cd97a2073648727291a00
Daniel Olivares <168047122+A01796061@users.noreply.github.com>
2025-01-25 18:18:06 -0600

    Add files via upload

--- SolucionSumaArreglosParalela.cpp
+++ SolucionSumaArreglosParalela.cpp
@@ -1,20 +1,55 @@
-// SolucionSumaArreglosParalela.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Author Daniel Olivares
+#include <cstdlib>
+#include <ctime>
+#include <iostream> 
+#include <random> 
 
-#include <iostream>
+#ifdef _OPENMP
+    #include <omp.h>
+#else
+    #define omp_get_thread_num() 0 
+#endif
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void assignRandomValuesToArrays(int arraySize, int* arrayA, int* arrayB) {
+    // Asignar valores aleatorios al arreglo usando OpenMP
+    #pragma omp parallel for 
+    for (int i = 0; i < arraySize; ++i) {
+        int threadId = omp_get_thread_num(); // Obtener el número de thread 
+        //Se obtiene un numero random utilizando el hardware y sumandole el 
+        // numero del thread esto va a garantizar que cada numero generado sea aleatorio 
+        // a travez de todos los threads
+        std::random_device rd;
+        std::mt19937 gen(rd() + threadId);
+        std::uniform_int_distribution<> distrib(1, arraySize);
+
+        arrayA[i] = distrib(gen);//Se generae un numero random para el arreglo A
+        arrayB[i] = distrib(gen);//Se generae un numero random para el arreglo B
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void sumArrays(int arraySize, int* arrayA, int* arrayB, int* arrayC) {
+    // Sumar los valores de los arreglos A y B en el arreglo arrayC
+    #pragma omp parallel for
+    for (int i = 0; i < arraySize; ++i) {
+        arrayC[i] = arrayA[i] + arrayB[i];
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void showArrayValues(int arraySize, int* arrayA, int* arrayB, int* arrayC) {
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << "ArrayA[" <<  i << "] = " << arrayA[i] << " + ArrayB[" <<  i << "] = " << arrayB[i] << " = ArrayC[" <<  i << "] " << arrayC[i] << std::endl;
+    }
+}
+
+int main() {
+    const int arraySize = 1000;  // Tamaño del arreglo
+    int arrayA[arraySize];
+    int arrayB[arraySize];
+    int arrayC[arraySize];
+
+    assignRandomValuesToArrays(arraySize, arrayA, arrayB);
+    sumArrays(arraySize, arrayA, arrayB, arrayC);
+    showArrayValues(arraySize, arrayA, arrayB, arrayC);
+
+    return 0;
+}
